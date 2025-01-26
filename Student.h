c15055ae0ece6e563029f89ed02de82cb18dd6ff
DylanPho <159408670+DylanPho@users.noreply.github.com>
2025-01-26 03:47:10 -0700

    seperating code into various header files

---
+++ Student.h
@@ -0,0 +1,15 @@
+// Header: Student.h
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <string>
+
+class Student {
+public:
+    std::string student_name;
+    int student_number;
+    std::string major;
+    int class_level;
+};
+
+#endif
