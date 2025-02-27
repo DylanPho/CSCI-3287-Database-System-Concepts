157cec0dea0a4e2b596cdf990f6e2f997ad3616b
DylanPho <159408670+DylanPho@users.noreply.github.com>
2025-02-27 13:30:07 -0700

    update

---
+++ Class Exercises/Invoice_example/Insert_category.sql
@@ -0,0 +1,16 @@
+--------------------------
+---Insert into CATEGORY---
+Insert into CATEGORY (Category_id, Category) values
+(1, 'Electronics'),
+(2, 'Furniture'),
+(3, 'Sports'),
+(4, 'Automotive'),
+(5, 'Books');
+
+--or--
+
+Insert into CATEGORY (Category_id, Category) values (1, 'Electronics');
+Insert into CATEGORY (Category_id, Category) values (2, 'Furniture');
+Insert into CATEGORY (Category_id, Category) values(3, 'Sports');
+Insert into CATEGORY (Category_id, Category) values(4, 'Automotive');
+Insert into CATEGORY (Category_id, Category) values(5, 'Books');
